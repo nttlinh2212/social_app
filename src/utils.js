6e2d76a0af2cfbe6e7d2e0844b348f73705a57cd
linhttnguyen <73452957+nttlinh2212@users.noreply.github.com>
2022-02-21 11:53:23 +0700

    update backend url

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: process.env.REACT_APP_API_URL,
     timeout: 5000
   });
   //console.log("axios",instance);
